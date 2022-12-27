bfd48c4614899bd8ade3e152541317ad816cdd46
Bharat Dangi <bharat@pop-os.localdomain>
2022-12-27 21:23:29 +0545

    Changes backend setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const colors = require("colors");
+const cors = require("cors");
 const dbConfig = require("./config/db.config");
 
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const app = express();
 
 //Connect to database
 dbConfig();
+app.use(cors());
 app.get("/", (req, res) => {
   res.status(200).send("Working");
 });
